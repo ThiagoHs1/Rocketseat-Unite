2d68420d6b4d147f8f2c54e409613e998cff42c1
Istan <thiago.sh26@gmail.com>
2024-04-07 18:48:01 -0300

    first commit

---
+++ src/main/java/rocketseat/com/passint/PassIntApplication.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocketseat/com/passint/PassIntApplicationTests.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
